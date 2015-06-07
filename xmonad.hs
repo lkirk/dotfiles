c31c9c002b5839a5e5258a1dbed07573a4265cff
lloydkirk <kirklloyd@gmail.com>
2015-06-07 01:54:29 -0700

    added xF86 Audio up down mute

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,20 @@
+{-# LANGUAGE RecordWildCards #-}
+
 import XMonad
 import XMonad.Hooks.DynamicLog
+import Graphics.X11.ExtraTypes.XF86
+import qualified Data.Map as M
 
-main = 
-  xmonad =<< xmobar defaultConfig
-    { terminal    = "xterm"
-    , borderWidth = 0
-    }
+main = xmonad =<< xmobar defaultConfig
+       { terminal    = "urxvt"
+       , borderWidth = 0
+       , keys = keys defaultConfig <+> \conf@(XConfig { .. })
+              -> M.fromList
+                [ ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 5+")
+                , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 5-")
+                , ((0, xF86XK_AudioMute),        spawn "amixer set Master toggle")
+                -- , ((modMask .|. shiftMask, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
+                -- , ((modMask .|. shiftMask, xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
+                -- , ((modMask .|. shiftMask, xF86XK_AudioMute),        spawn "amixer set Speaker toggle")
+                ]
+       }
